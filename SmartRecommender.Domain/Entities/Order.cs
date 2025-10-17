0f7a762b6a9713996d1f2984790ab05a4512b1e3
ZeynabNadiDev <znadi05@gmail.com>
2025-10-17 14:57:56 -0700

    Finalize Domain Entities (User, Product, Order)

---
+++ SmartRecommender.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRecommender.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public ulong UserId { get; set; }
+        public User User { get; set; } = null!;
+        public ICollection<Product> Products { get; set; }=new List<Product>();
+        public decimal TotalOrder { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public int? Rating { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ SmartRecommender.Domain/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRecommender.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal? Discount { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public ICollection<User> FavoriteByUsers { get; set; } = new List<User>();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public double? AverageRating { get; set; }
+        public int PopularityScore { get; set; }
+
+
+
+    }
+}
